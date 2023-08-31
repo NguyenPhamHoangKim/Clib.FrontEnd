4f75b5e2e9a5199cce883b4b563a733b52e716a2
Khan <nguyenhoangkhan210@gmail.com>
2023-08-31 21:56:45 +0700

    first commit, init base

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        branding: {
+          DEFAULT: "#1A75FF",
+          secondary: "#0ABFAC",
+        },
+        light: {
+          blue: "#EBF7FC",
+        },
+        grey: {
+          200: "#EBEEF0",
+          300: "#A1ABB2",
+          400: "#52616B",
+        },
+        "neutral-dark": {
+          500: "#2C2F33",
+        },
+      },
+      fontFamily: {
+        Poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+        Inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
