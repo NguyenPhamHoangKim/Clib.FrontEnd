b42302f8958998618339e611a4db08dd8592b7ab
Khan <nguyenhoangkhan210@gmail.com>
2023-09-05 19:46:13 +0700

    checkClickOutside

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+function checkClickOutside(elementId, callback) {
+  $(document).on("click", function (event) {
+    const targetElement = event.target;
+    const containerElement = $("#" + elementId);
+    if (containerElement.has(targetElement).length === 0) {
+      callback();
+    }
+  });
+}
+
 $("#toggle-mobile-menu").click(function () {
   $("#header-tab-modal").addClass("active");
 });
@@ -6,15 +16,27 @@ $("#close-header-tab-modal").click(function () {
   $("#header-tab-modal").removeClass("active");
 });
 
-$("#toggle-notification").click(function () {
-  $(this)
-    .find(".noti-icon")
-    .attr(
-      "src",
-      $(this).find("img").attr("src") ===
-        "/assets/svgs/notification-selected.svg"
-        ? "/assets/svgs/notification-pc.svg"
-        : "/assets/svgs/notification-selected.svg",
-    );
+const handleToggleNoti = function () {
+  const $elem = $("#noti-wrapper .noti-icon");
   $("#noti-popup").toggleClass("active");
+
+  $elem.attr(
+    "src",
+    $elem.attr("src") === "/assets/svgs/notification-selected.svg"
+      ? "/assets/svgs/notification-pc.svg"
+      : "/assets/svgs/notification-selected.svg",
+  );
+};
+
+const handleCloseNoti = function () {
+  $("#noti-popup").removeClass("active");
+  $("#noti-wrapper .noti-icon").attr("src", "/assets/svgs/notification-pc.svg");
+};
+
+$("#toggle-notification").click(function () {
+  handleToggleNoti();
+});
+
+checkClickOutside("noti-wrapper", function () {
+  handleCloseNoti();
 });
