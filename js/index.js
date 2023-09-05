244218595816cc47e801b265931b6ef77ffb7d27
Khan <nguyenhoangkhan210@gmail.com>
2023-09-05 17:48:13 +0700

    u

--- js/index.js
+++ js/index.js
@@ -5,3 +5,16 @@ $("#toggle-mobile-menu").click(function () {
 $("#close-header-tab-modal").click(function () {
   $("#header-tab-modal").removeClass("active");
 });
+
+$("#toggle-notification").click(function () {
+  $(this)
+    .find(".noti-icon")
+    .attr(
+      "src",
+      $(this).find("img").attr("src") ===
+        "/assets/svgs/notification-selected.svg"
+        ? "/assets/svgs/notification-pc.svg"
+        : "/assets/svgs/notification-selected.svg",
+    );
+  $("#noti-popup").toggleClass("active");
+});
