7c5e594698a7cd590365a51a17db8b8e7095ad00
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2023-09-16 14:20:42 +0700

    marketplace monile

--- js/maketplace.js
+++ js/maketplace.js
@@ -74,12 +74,124 @@ $(document).ready(function () {
   });
 
   $("#button-filter").click(function () {
-    // Show the video section and hide the music section
     $(".filter-music").show();
   });
   $(".close-filter").click(function () {
-    // Show the video section and hide the music section
     $(".filter-music").hide();
     $(".filter-product").hide();
+  });
+
+  $(".button").click(function () {
+    $(".button").removeClass("active-mobile");
+
+    $(this).addClass("active-mobile");
+  });
+
+  $(".hero").click(function () {
+    $(".hero").removeClass("active-hero");
+
+    $(this).addClass("active-hero");
+  });
+
+  $(".video-mobile").hide();
+
+  // Xử lý sự kiện khi nút "Musics" được nhấp
+  $(".music").click(function () {
+    // Ẩn nội dung video
+    $(".video-mobile").hide();
+
+    // Hiển thị nội dung âm nhạc
+    $(".music-mobile").show();
+  });
+
+  // Xử lý sự kiện khi nút "Videos" được nhấp
+  $(".video").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".music-mobile").hide();
+
+    // Hiển thị nội dung video
+    $(".video-mobile").show();
+  });
+
+  $(".service").hide();
+  $(".yourshop").hide();
+
+  $(".btn-digital").click(function () {
+    // Ẩn nội dung video
+    $(".digital").show();
+
+    // Hiển thị nội dung âm nhạc
+    $(".service").hide();
+    $(".yourshop").hide();
+  });
+
+  // Xử lý sự kiện khi nút "Videos" được nhấp
+  $(".btn-service").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".service").show();
+
+    // Hiển thị nội dung video
+    $(".digital").hide();
+    $(".yourshop").hide();
+  });
+
+  $(".btn-yourshop").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".yourshop").show();
+
+    // Hiển thị nội dung video
+    $(".service").hide();
+    $(".digital").hide();
+  });
+
+  $(".filter-digital").hide();
+  $(".filter-service").hide();
+
+  $(".filter-music-mobile").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".filter-digital").show();
+
+    // Hiển thị nội dung video
+  });
+  $(".filter-service-mobile").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".filter-service").show();
+
+    // Hiển thị nội dung video
+  });
+  $(".hiden-filter").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".filter-digital").hide();
+
+    // Hiển thị nội dung video
+  });
+  $(".hidden-filter-mobile").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".filter-service").hide();
+  });
+  $(".upload").hide();
+
+  $(".yourshop").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".upload").show();
+  });
+  $(".close-upload").click(function () {
+    // Ẩn nội dung âm nhạc
+    $(".upload").hide();
+  });
+
+  new Swiper(".swiper-digital", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    slidesPerView: "auto",
+    spaceBetween: 10, // Khoảng cách giữa các slide
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2, // Hiển thị 2 slide
+      },
+      640: {
+        slidesPerView: 2.2, // Hiển thị 2 slide
+      },
+    }, // Adjust spacing between slides
   });
 });
