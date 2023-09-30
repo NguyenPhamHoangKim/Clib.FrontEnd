5cc3ecc26a32722238fc243c50c0bcc8929626b2
Khan <nguyenhoangkhan210@gmail.com>
2023-09-30 23:33:02 +0700

    u

--- js/feed.js
+++ js/feed.js
@@ -19,3 +19,39 @@ $(".close-modal").each(function () {
     $(".modal-report").hide();
   });
 });
+
+$(".show-liked-by-modal").each(function () {
+  checkClickOutside(
+    "liked-by-modal--inner",
+    function () {
+      $("#liked-by-modal").removeClass("!flex");
+    },
+    "show-liked-by-modal",
+  );
+
+  $(this).click(function () {
+    $("#liked-by-modal").toggleClass("!flex");
+  });
+});
+
+$("#liked-by-close").click(function () {
+  $("#liked-by-modal").removeClass("!flex");
+});
+
+$(".show-reposted-by-modal").each(function () {
+  checkClickOutside(
+    "reposted-by-modal--inner",
+    function () {
+      $("#reposted-by-modal").removeClass("!flex");
+    },
+    "show-reposted-by-modal",
+  );
+
+  $(this).click(function () {
+    $("#reposted-by-modal").toggleClass("!flex");
+  });
+});
+
+$("#reposted-by-close").click(function () {
+  $("#reposted-by-modal").removeClass("!flex");
+});
