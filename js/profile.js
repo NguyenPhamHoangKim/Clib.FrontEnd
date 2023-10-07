6b3770b0ed14e52dcc385febc2bd3f757bd655bc
Khan <nguyenhoangkhan210@gmail.com>
2023-10-07 11:10:47 +0700

    fb

--- js/profile.js
+++ js/profile.js
@@ -1,64 +1,60 @@
-$(".profile-nav:not(.active)").each(function () {
-  $(this).mouseenter(function () {
-    const originalSrc = $(this).find("img").attr("src");
-    const newSrc = originalSrc.replace(".svg", "-selected.svg");
-    $(this).find("img").attr("src", newSrc);
+if ($(".edit-profile-group")) {
+  $(".edit-profile-group").each(function () {
+    const editProfileGroup = $(this);
+    editProfileGroup.find(".profile-edit-btn").click(function () {
+      editProfileGroup.find(".save-edit-profile").addClass("show");
+      editProfileGroup.find("input, textarea").removeAttr("disabled");
+    });
+
+    editProfileGroup.find(".close-save-edit-profile").click(function () {
+      $(this).parent().parent().removeClass("show");
+      editProfileGroup.find("input, textarea").attr("disabled", "disabled");
+    });
   });
-});
-
-$(".profile-nav:not(.active)").mouseleave(function () {
-  const originalSrc = $(this).find("img").attr("src");
-  const newSrc = originalSrc.replace("-selected.svg", ".svg");
-  $(this).find("img").attr("src", newSrc);
-});
-
-$(".edit-profile-group").each(function () {
-  const editProfileGroup = $(this);
-  editProfileGroup.find(".profile-edit-btn").click(function () {
-    editProfileGroup.find(".save-edit-profile").addClass("show");
-    editProfileGroup.find("input, textarea").removeAttr("disabled");
+}
+if ($("#show-edit-modal")) {
+  $("#show-edit-modal").click(function () {
+    $("#edit-profile--modal").addClass("show");
   });
+}
 
-  editProfileGroup.find(".close-save-edit-profile").click(function () {
-    $(this).parent().parent().removeClass("show");
-    editProfileGroup.find("input, textarea").attr("disabled", "disabled");
+if ($("#edit-profile--modal")) {
+  $("#edit-profile--modal").click(function () {
+    $(this).removeClass("show");
   });
-});
+}
 
-$("#show-edit-modal").click(function () {
-  $("#edit-profile--modal").addClass("show");
-});
-
-$("#edit-profile--modal").click(function () {
-  $(this).removeClass("show");
-});
-
-$("#edit-profile--inner").click(function (e) {
-  e.stopPropagation();
-});
-
-$(".gallery-img").each(function () {
-  $(this).click(function () {
-    $("#gallery-focus-image").toggleClass("show");
+if ($("#edit-profile--inner")) {
+  $("#edit-profile--inner").click(function (e) {
+    e.stopPropagation();
   });
-});
-
-$("#close-focus-img").click(function () {
-  $("#gallery-focus-image").removeClass("show");
-});
-
-$(".comment-gallery-item").each(function () {
-  const _this = $(this);
-
-  _this.find(".show-reply-box").click(function () {
-    $(".reply-cmt-box.show").removeClass("show");
-    const replyBox = $(this)
-      .closest(".cmt-box-right")
-      .parent()
-      .next(".reply-cmt-box");
-    replyBox.addClass("show");
+}
+if ($(".gallery-img")) {
+  $(".gallery-img").each(function () {
+    $(this).click(function () {
+      $("#gallery-focus-image").toggleClass("show");
+    });
   });
-});
+}
+if ($("#close-focus-img")) {
+  $("#close-focus-img").click(function () {
+    $("#gallery-focus-image").removeClass("show");
+  });
+}
+if ($(".comment-gallery-item")) {
+  $(".comment-gallery-item").each(function () {
+    const _this = $(this);
+
+    _this.find(".show-reply-box").click(function () {
+      $(".reply-cmt-box.show").removeClass("show");
+      const replyBox = $(this)
+        .closest(".cmt-box-right")
+        .parent()
+        .next(".reply-cmt-box");
+      replyBox.addClass("show");
+    });
+  });
+}
 
 new Swiper(".goods-swiper", {
   slidesPerView: 2.5,
@@ -70,6 +66,28 @@ new Swiper(".goods-swiper", {
   autoResize: false,
 });
 
+new Swiper("#new-swiper", {
+  slidesPerView: 2.5,
+  spaceBetween: 12,
+  navigation: {
+    nextEl: ".swiper-new-next",
+    prevEl: ".swiper-new-prev",
+  },
+  autoResize: false,
+  breakpoints: {
+    480: {
+      slidesPerView: 3.5,
+    },
+    680: {
+      slidesPerView: 4.5,
+      spaceBetween: 14,
+    },
+    1024: {
+      slidesPerView: 6.5,
+      spaceBetween: 16,
+    },
+  },
+});
 new Swiper(".albums-swiper", {
   slidesPerView: 2.5,
   spaceBetween: 12,
